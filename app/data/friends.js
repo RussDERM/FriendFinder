08990604edcf827e2e8ec3a3436b16527ff3fe8e
Russ <russ.dermody@gmail.com>
2019-05-21 12:06:33 -0700

    Put preliminary code in friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+// /////////////////////////////////////////////////////
+// This file holds all of the stored 'friend' data
+// /////////////////////////////////////////////////////
+
+var friendsList = [
+  {
+    friendName: 'Sasquatch',
+    photo: 'https://nu.aeon.co/images/fb911487-7bbe-4752-b655-3c8b0d745801/idea_sized-bigfoot_gimli_patterson.jpg',
+    scores: [5, 4, 1, 2, 3, 4, 5, 5, 4, 3]
+  },
+  {
+    friendName: 'King Kong',
+    photo: 'https://cdn.vox-cdn.com/thumbor/T1OW_ofaAPK3zZzq588r5zvCdJA=/0x0:2864x1200/1200x800/filters:focal(776x510:1234x968)/cdn.vox-cdn.com/uploads/chorus_image/image/53642493/kongyell.0.jpg',
+    scores: [4, 3, 2, 1, 3, 4, 5, 5, 4, 3]
+  }
+
